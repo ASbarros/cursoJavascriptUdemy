1dd87c885797a49cfaf08f613c19f7e577f1f287
asbarros <anderson.barros0298@gmail.com>
2019-10-06 17:23:12 -0300

    CRUD em uma API em node.js (projeto parte 2 )

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -2,12 +2,45 @@ const porta = 3003;
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const bancoDeDados = require('./bancoDeDados');
+
+app.use(bodyParser.urlencoded({
+    // para todas as requisições esse metodo é chamado,
+    // responsavel por transformar o corpo da requisição em um objeto...
+    extended: true
+}));
 
 app.get('/produtos', (req, res, next) => {
-    res.send({
-        nome: 'Notebook',
-        preco: 123.45
-    }); // Converte para JSON...
+    // pegando todos os produtos...
+    res.send(bancoDeDados.getProdutos());
+});
+
+app.get('/produtos/:id', (req, res, next) => {
+    // pegando um produto expecifico...
+    res.send(bancoDeDados.getProduto(req.params.id));
+});
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    });
+    res.send(produto) // JSON...
+});
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id:req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    });
+    res.send(produto) // JSON...
+});
+
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id);
+    res.send(produto) // JSON...
 });
 
 app.listen(porta, () => {
