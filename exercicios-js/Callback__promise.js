23d36d2b9e702a07e435c2002e0aeff683e9d3fb
asbarros <anderson.barros0298@gmail.com>
2019-10-11 16:15:14 -0300

    promise

---
+++ exercicios-js/Callback__promise.js
@@ -0,0 +1,73 @@
+// sem promise...
+const http = require('http')
+
+const getTurma = (letra, callback) => {
+    const url = `http://files.cod3r.com.br/curso-js/turma${letra}.json`
+    http.get(url, res => {
+        let resultado = ''
+
+        res.on('data', dados => {
+            resultado += dados
+        })
+
+        res.on('end', () => {
+            callback(JSON.parse(resultado))
+        })
+    })
+}
+
+let nomes = []
+getTurma('A', alunos => {
+    nomes = nomes.concat(alunos.map(a => `A: ${a.nome}`))
+    getTurma('B', alunos => {
+        nomes = nomes.concat(alunos.map(a => `B: ${a.nome}`))
+        getTurma('C', alunos => {
+            nomes = nomes.concat(alunos.map(a => `C: ${a.nome}`))
+            console.log(nomes)
+        })
+    })
+})
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// usando promise...
+const http2 = require('http')
+
+const getTurma2 = letra => {
+    const url = `http://files.cod3r.com.br/curso-js/turma${letra}.json`
+    return new Promise((resolve, reject) => {
+        http2.get(url, res => {
+            let resultado = ''
+
+            res.on('data', dados => {
+                resultado += dados
+            })
+
+            res.on('end', () => {
+                try {
+                    resolve(JSON.parse(resultado))
+                } catch (error) {
+                    reject(error)
+                }
+            })
+        })
+    })
+}
+
+nomes = []
+getTurma2('A').then(alunos => {
+    nomes = nomes.concat(alunos.map(a => `A: ${a.nome}`))
+    getTurma2('B').then(alunos => {
+        nomes = nomes.concat(alunos.map(a => `B: ${a.nome}`))
+        getTurma2('C').then(alunos => {
+            nomes = nomes.concat(alunos.map(a => `C: ${a.nome}`))
+            console.log(nomes)
+        })
+    })
+})
+
+Promise.all([getTurma2('A'), getTurma2('B'), getTurma2('C')])
+    .then(turmas => [].concat(...turmas))
+    .then(alunos => alunos.map(aluno => aluno.nome))
+    .then(nomes => console.log(nomes))
+    .catch(e => console.log(e.message))
