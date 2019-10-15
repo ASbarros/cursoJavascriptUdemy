c7741f9bd5d9aef1e7f2980d277526c938c9c128
asbarros <anderson.barros0298@gmail.com>
2019-10-15 10:52:35 -0300

    segunda parte

--- ajax/server.js
+++ ajax/server.js
@@ -8,5 +8,30 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(bodyParser.json())
 
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './upload')
+    },
+    filename: function (req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({
+    storage
+}).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if (err) {
+            return res.end("Ocorreu um erro.")
+        }
+
+        res.end('Cocluído com sucesso.')
+    })
+})
+
 app.get('/teste', (req, res) => res.send('Ok'))
 app.listen(8080, () => console.log('Executando...'))
